4d9eb1bf3e751e1cb4f336cf1f9e0292cb6a2d0f
mcaron994-ux <mcaron994@gmail.com>
2026-09-05 13:40:25 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "fais pas Création/Expert",     // this is what people actually see
+        state: "Fais pas ⚙️Création/Expert",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
